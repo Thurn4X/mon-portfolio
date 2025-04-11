6a78b7a3fe39c59c22f98572c862a5774703da32
William Masih <william.masih@cpe.fr>
2025-04-11 16:21:18 +0200

    Ajouter composant Landing pour la page d'accueil

---
+++ mon-portfolio/src/components/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Bienvenue sur mon portfolio</h1>
+    </div>
+  );
+};
+
+export default Landing;
